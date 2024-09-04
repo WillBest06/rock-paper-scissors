5b40ba7a1c38652bbfb7892af46a8256fbeb567e
William Best <williambest133@gmail.com>
2024-09-04 16:44:57 +0100

    create new function playGame

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
+
 function getComputerChoice () {
     let randomNum = Math.floor(Math.random() * 100);
     let result = null;
@@ -21,3 +26,47 @@ function getHumanChoice() {
         getHumanChoice();
     }
 }
+
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        alert("Tie. No points awarded.");
+    } else if (computerChoice === "rock" && humanChoice === "paper") {
+        alert("You win! You have been awarded 1 point");
+        humanScore++;
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        alert("You lose! The computer has been awarded 1 point");
+        computerScore++;
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        alert("You lose! The computer has been awarded 1 point");
+        computerScore++;
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        alert("You win! You have been awarded 1 point");
+        humanScore++;
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        alert("You win! You have been awarded 1 point");
+        humanScore++;
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        alert("You lose! The computer has been awarded 1 point");
+        computerScore++;
+    }
+}
+
+function playGame() {
+    let roundsPlayed = 0;
+    
+    for (roundsPlayed = 0; roundsPlayed < 5; roundsPlayed++) {
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+        playRound(computerSelection, humanSelection);
+    }
+
+    if (humanScore > computerScore) {
+        alert("Congrats, you won!");
+    } else if (humanScore < computerScore) {
+        alert("Unlucky, you lost!");
+    } else if (humanScore === computerScore) {
+        alert("Wow, you tied!");
+    }
+}
+
+playGame();
